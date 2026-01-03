fab74f6d6cdc2db6eb582c17e7c4c1251919d2a2
bady55358yw <bady55358yw@gmail.com>
2026-01-03 10:44:01 +0800

    feat: 將預設頁面指定為 home，並且在 home 頁面 redirect 至 login 頁面

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,7 @@
-import {
-  type RouteConfig,
-  layout,
-  route
-} from "@react-router/dev/routes";
+import { type RouteConfig, layout, route,index } from "@react-router/dev/routes";
 
 export default [
-  //   index("routes/home.tsx"),
+  index("./routes/home.tsx"),
 
   layout("./layouts/BaseLayout/BaseLayout.tsx", [
     route("login", "./routes/auth/login.tsx"),
